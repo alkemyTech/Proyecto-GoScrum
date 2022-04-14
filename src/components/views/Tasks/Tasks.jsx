09b8da7d1db5a9dd1c647b09f893f70680cf149e
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-14 04:32:17 -0300

    Clase 3 - Lista de tareas mobile first

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,2 +1,49 @@
 import { Header } from "../../Header/Header";
-export const Tasks = () => <Header />;
+
+import "./Tasks.styles.css";
+
+export const Tasks = () => (
+  <>
+    <Header />
+    <main id="tasks">
+      <div className="list_header">
+        <h2>Mis Tareas</h2>
+      </div>
+      <div className="list">
+        <div className="card">
+          <div className="close">x</div>
+          <h3>Tarea 1</h3>
+          <h6>24/1/2022 16:40 hs.</h6>
+          <h5>Facundo Uferer</h5>
+          <button type="button">Nueva</button>
+          <button type="button">Alta</button>
+          <p>Descripción fake</p>
+        </div>
+      </div>
+
+      <div className="list">
+        <div className="card">
+          <div className="close">x</div>
+          <h3>Tarea 1</h3>
+          <h6>24/1/2022 16:40 hs.</h6>
+          <h5>Facundo Uferer</h5>
+          <button type="button">Nueva</button>
+          <button type="button">Alta</button>
+          <p>Descripción fake</p>
+        </div>
+      </div>
+
+      <div className="list">
+        <div className="card">
+          <div className="close">x</div>
+          <h3>Tarea 1</h3>
+          <h6>24/1/2022 16:40 hs.</h6>
+          <h5>Facundo Uferer</h5>
+          <button type="button">Nueva</button>
+          <button type="button">Alta</button>
+          <p>Descripción fake</p>
+        </div>
+      </div>
+    </main>
+  </>
+);
