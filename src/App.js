d09f4da72c124c18eb13a0e799e291461e990973
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-14 03:21:02 -0300

    Clase 3: Páginas privadas y públicas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
 import './App.css';
 import { Login } from './components/views/Login/Login';
@@ -6,9 +6,21 @@ import { Register } from './components/Register/Register';
 import { Tasks } from './components/views/Tasks/Tasks';
 import { Error404 } from './components/views/Error404/Error404';
 
+const RequireAuth = ({ children }) => {
+  if (!localStorage.getItem("logged")) {
+    return <Navigate to="/login" replace={true} />
+  }
+  return children
+}
+
 export const App = () =>
   <Routes>
+    <Route path="/"
+      element={
+        <RequireAuth>
+          <Tasks />
+        </RequireAuth>
+      } />
     <Route path="/login" element={<Login />} />
-    <Route path="/" element={<Tasks />} />
     <Route path="*" element={<Error404 />} />
   </Routes>
