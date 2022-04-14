e148769b2f5855e5d481c00ae29bc2b229df28dd
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-14 01:05:32 -0300

    Clase 3 Login con mobile first

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
 import './App.css';
 
-export const App = () => <div>Go Scrum</div>;
+export const App = () => (
+  <div className="container">
+    <form>
+      <h1>Iniciar sesión</h1>
+      <div>
+        <label>Email</label>
+        <input name="email" type="text" />
+      </div>
+      <div>
+        <label>Contraseña</label>
+        <input name="password" type="password" />
+      </div>
+      <div>
+        <button type="submit">Enviar</button>
+      </div>
+    </form>
+  </div>
+)
