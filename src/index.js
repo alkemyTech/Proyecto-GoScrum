a925d54b62ec3d51d37aa1883e638d0209d82d7f
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-17 18:58:02 -0300

    Clase 2 Store y reducer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux"
 
 import './index.css';
+import { store } from "./store/store"
 import { App } from './App';
 
 render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/store/reducers/tasksReducer.js
@@ -0,0 +1,9 @@
+const initialState = {
+   loading: false,
+   tasks: [],
+   error: "",
+}
+
+export const tasksReducer = (state = initialState) => {
+   return state
+}
