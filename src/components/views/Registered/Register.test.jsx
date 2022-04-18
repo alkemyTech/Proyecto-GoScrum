733d7135ed9cd9b7c14f354012a6957d528df096
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-17 21:58:45 -0300

    Clase 2 Mockear peticiones a API

--- src/components/views/Registered/Register.test.jsx
+++ src/components/views/Registered/Register.test.jsx
@@ -1,25 +1,31 @@
-import { MemoryRouter } from "react-router-dom"
-import { render, screen } from "react-dom"
-import { Register } from "./Register"
-import { rest } from "msw"
-import { setupServer } from "msw/node"
+import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "react-dom";
+import { Register } from "./Register";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
 const server = setupServer(
-   rest.get("http:localhost:8080/auth/data", (req, res, ctx) => {
-      return res(
-         ctx.json({
-            result: {
-               contiente: ["America", "Europa", "Otro"],
-               registro: ["Otro", "Latam", "Brasil", "America del Norte"],
-               Rol: ["Team Member", "Team Leader"],
-            },
-         })
-      )
-   })
-)
+  rest.get("http:localhost:8080/auth/data", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        result: {
+          contiente: ["America", "Europa", "Otro"],
+          registro: ["Otro", "Latam", "Brasil", "America del Norte"],
+          Rol: ["Team Member", "Team Leader"],
+        },
+      })
+    );
+  })
+);
 
 it("fetch role options", async () => {
-   render(<Register />, { wrapper: MemoryRouter })
+  render(<Register />, { wrapper: MemoryRouter });
 
-   await screen.findByRole("option", { name: "America" })
-})
+  expect(
+    screen.getByRole("option", { name: "Seleccionar roo..." })
+  ).toBeInTheDocument();
+
+  expect(
+    await screen.findByRole("option", { name: "Europa" })
+  ).toBeInTheDocument();
+});
