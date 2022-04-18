5793b11ab6266e51a07976900037de8b320c1319
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-17 21:40:09 -0300

    Clase 1 Tests unitarios de renderizado

--- src/components/views/Donate/Donate.test.jsx
+++ src/components/views/Donate/Donate.test.jsx
@@ -1,12 +1,17 @@
-import { render, screen } from "react-dom"
-import { Donate } from "./Donate"
+import { render, screen } from "react-dom";
+import { Donate } from "./Donate";
 
 it("renderizado h1 correctamente", () => {
-   render(<Donate />)
-   expect(
-      screen.getByRole("heading", {
-         name: "Colaborá con el proyecto",
-         level: 1,
-      })
-   ).toBeInTheDocument()
-})
+  render(<Donate />);
+  expect(
+    screen.getByRole("heading", {
+      name: "Colaborá con el proyecto",
+      level: 1,
+    })
+  ).toBeInTheDocument();
+});
+
+it("renderizado un a", () => {
+  render(<Donate />);
+  expect(screen.getByRole("link")).toHaveAttribute("href");
+});
