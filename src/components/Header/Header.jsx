3a44d5d84f0ec2991fdc1242812579dd0bc91da5
Facundo Uferer <juanfacundouf@gmail.com>
2022-04-17 21:27:10 -0300

    Clase 7 Consumiendo el estado desde varios componentes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import "./Header.styles.css";
 
 export const Header = () => {
   const navigate = useNavigate();
+
+  const { tasks } = useSelector((state) => {
+    return state.tasksReducer;
+  });
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -15,6 +20,7 @@ export const Header = () => {
     <header>
       <img src="/img/goscrum.png" alt="Logo" />
       <div className="wrapper_right_header">
+        <div className="black">Tareas creadas: {!tasks ? 0 : tasks.length}</div>
         <div className="black">{localStorage.getItem("userName")}</div>
         <div onClick={handleLogout}>x</div>
       </div>
